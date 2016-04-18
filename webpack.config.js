66277b05d704a945098bce0c7e4625764fb503d4
Inácio Schweller <iscmendonca@gmail.com>
2016-04-18 08:40:41 -0300

    adding changes to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
+  entry: __dirname + "/js/map.js",
+  output: {
+    path: __dirname + "/js",
+    filename: "bundle.js"
+  },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, "./js/")
+        ],
+        loader: "babel-loader"
+      }
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-          warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      NODE_ENV: "production"
-    })
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin()
   ]
 }
